77d212181b048f0b412b73d735676729c546b3bc
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-02-27 10:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+	event.preventDefault();  
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    const valRadius = parseFloat(document.getElementById('radius').value);
+    //Write your code here
+	if(isNaN(valRadius) || valRadius<0){
+		getElementById("volume").textContent = "invalid input";
+		return;
+  }
+	const calculation = (4/3)*Math.PI*Math.pow(valRadius,3);
+	document.getElementById("volume").textContent = calculation;
+} 
+window.onload = function(){
+	document.getElementById('MyForm').onsubmit = volume_sphere;
+}
